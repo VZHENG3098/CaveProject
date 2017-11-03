6c43edada8605d42c624effb13b0d2d4b06706f7
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-03 15:14:41 -0400

    Fixed minigame constructor and started runGame()

--- CaveProject/src/bendimitris/DimitrisBenMinigame.java
+++ CaveProject/src/bendimitris/DimitrisBenMinigame.java
@@ -3,14 +3,43 @@ package bendimitris;
 public class DimitrisBenMinigame 
 {
 	private String[][] board;
+	private boolean playing;
 	
 	public DimitrisBenMinigame()
 	{
-		board = new String[3][7];
-		setUpBoard();
-		printBoard();
+		board = new String[4][7];
+		playing = true;
 	}
 	
+	private void runGame() 
+	{
+		// generate random place in line to open a space every other turn, have the teacher
+		// face a random direction every turn, add one to skip for every person you skip
+		int roundNumber = 0;
+		
+		// every other round
+		if (roundNumber % 2 == 0)
+		{
+			int openingY = (int)(Math.random() * board.length);
+			int openingX;
+			if (openingY > 0)
+			{
+				openingX = 1;
+			}
+			else
+			{
+				openingX = (int)(Math.random() * board[openingY].length);
+			}
+			
+			if (board[openingY][openingX].equals("X"))
+			{
+				runGame();
+			}
+		}
+		
+		
+	}
+
 	public void printBoard()
 	{
 		for (int y = 0; y < board.length; y += 1)
@@ -55,6 +84,10 @@ public class DimitrisBenMinigame
 						board[y][x] = " ";
 					}
 				}
+				else if (y == 3)
+				{
+					board[y][x] = " ";
+				}
 				else
 				{
 					if (x == 3)
@@ -67,6 +100,17 @@ public class DimitrisBenMinigame
 					}
 				}
 			}
+		}
+	}
+	
+	public void startGame()
+	{
+		setUpBoard();
+		printBoard();
+		
+		while (playing)
+		{
+			runGame();
 		}
 	}
 }
