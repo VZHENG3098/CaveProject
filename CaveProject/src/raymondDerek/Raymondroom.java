ec781a1aa357abcdaa93fa4e4caf273943daf5cc
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-03 14:50:47 -0400

    212112

---
+++ CaveProject/src/raymondDerek/Raymondroom.java
@@ -0,0 +1,12 @@
+package raymondDerek;
+
+import caveExplorer.NPCRoom;
+
+public class Raymondroom extends NPCRoom {
+
+	public Raymondroom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
