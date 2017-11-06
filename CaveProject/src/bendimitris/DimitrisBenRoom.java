e7d16f34b1debd81abdb5ed542b3092db8a2bdcd
deltaalpha33 <dalexakos@gmail.com>
2017-11-06 14:31:04 -0500

    added user input to cafeteria

--- CaveProject/src/bendimitris/DimitrisBenRoom.java
+++ CaveProject/src/bendimitris/DimitrisBenRoom.java
@@ -24,6 +24,32 @@ public class DimitrisBenRoom extends CaveRoom {
 		
 	}
 	
+	public void interpretInput(String input) {
+		while(!isValid(input)) {
+			printValidMoves();
+			
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = validMoves().indexOf(input);
+		if (direction < 4) {
+			goToRoom(direction);
+		}else {
+			performAction(direction);
+		}
+	}
+	
+	public String validMoves() {
+		return "wdsae";
+	}
+	
+	public boolean isValid(String input) {
+		return input.length() == 1 && "wsade".indexOf(input) !=-1;
+	}
+	
+	public void printValidMoves() {
+		System.out.println("You can only enter 'w', 'a', 's','d', or 'e'.");
+	}
+	
 	public void performAction(int direction) {
 		if(direction == 4) {
 			//set up board before hand
