737e1585ec00ce0385128950512ec5172557569a
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-06 15:21:21 -0500

    STuff

---
+++ CaveProject/src/DavidVin/vincentRoom.java
@@ -0,0 +1,38 @@
+package DavidVin;
+
+import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
+
+public class vincentRoom extends CaveRoom {
+
+	public vincentRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void printValidMoves() {
+		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move "
+				+ "or you can press 'e' to interact.");
+	}
+
+	/**
+	 * override to add more moves
+	 * @return
+	 */
+	public String validMoves() {
+		return "wdsae";
+	}
+
+	/**
+	 * override to create response to keys other than wdsa
+	 * @param direction
+	 */ 
+	public void performAction(int direction) {
+		if(direction == 4) {
+			CaveExplorer.print("Press e to heal your player");
+			CaveExplorer.inventory.setHP();
+		}else {
+			CaveExplorer.print("That key does nothing");
+		}
+	}
+}
