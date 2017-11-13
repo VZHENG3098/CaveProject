b44e10de04e52697f2da0fa242db9cc404f294d2
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-13 15:31:05 -0500

    Updated BackEnd

--- CaveProject/src/raymondDerek/RaymondBackEnd.java
+++ CaveProject/src/raymondDerek/RaymondBackEnd.java
@@ -6,9 +6,20 @@ public class RaymondBackEnd implements DerekSupporter{
 	private	RaymondSupporter frontend;
 	private int hp;
 	private int level; //difficulty
+	private int balls;
 	
 	public RaymondBackEnd(RaymondSupporter frontend) {
 		this.frontend = frontend;
+		hp = 100; 
+		level = 1;
+	}
+	
+	private int getHp() {
+		return hp;
+	}
+	
+	private int getLevel() {
+		return level;
 	}
 
 }
