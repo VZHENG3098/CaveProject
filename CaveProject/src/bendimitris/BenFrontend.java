7e03b5827e67f69afb31ce98e7a1eb554cb7fde4
bdardia <bdardia6930@bths.edu>
2017-11-14 15:12:45 -0500

    made changes

--- CaveProject/src/bendimitris/BenFrontend.java
+++ CaveProject/src/bendimitris/BenFrontend.java
@@ -1,13 +1,1 @@
-package bendimitris;
-
-public class BenFrontend implements DimitrisSupport{
-	BenSupport backend;
-	
-	public BenFrontend(BenSupport backend) {
-		this.backend = backend;
-	}
-	
-	public static void main(String[] args) {
-
-	}
-}
+test
