1e22b625cd3c7ba37075502a15b368b0c53801b2
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-14 14:38:36 -0500

    Completed outline

--- CaveProject/src/bendimitris/DimitrisSupport.java
+++ CaveProject/src/bendimitris/DimitrisSupport.java
@@ -3,7 +3,9 @@ package bendimitris;
 public interface DimitrisSupport {
 	
 	public void printBoard();
+	public void setUpBoard();
+	public void moveTeacher();
 	public void startGame();
-	public void getObstacles();
+	public String[][] getBoard();
 
 }
