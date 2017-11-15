06168f7178046c44a07273bb66acae619b2b0b21
deltaalpha33 <dalexakos@gmail.com>
2017-11-15 14:37:47 -0500

    created method stubs

--- CaveProject/src/bendimitris/DimitrisBackend.java
+++ CaveProject/src/bendimitris/DimitrisBackend.java
@@ -6,5 +6,35 @@ public class DimitrisBackend implements BenSupport {
 	public DimitrisBackend(DimitrisSupport frontend) {
 		this.frontend = frontend;
 	}
+
+	@Override
+	public void runGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movePeople() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startTimer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean outOfTime() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
