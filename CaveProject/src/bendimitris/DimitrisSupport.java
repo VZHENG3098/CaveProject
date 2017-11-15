3063d285c356b6fa4bc5051f98f95971a9d7f253
deltaalpha33 <dalexakos@gmail.com>
2017-11-15 14:39:29 -0500

    fixed interface params

--- CaveProject/src/bendimitris/DimitrisSupport.java
+++ CaveProject/src/bendimitris/DimitrisSupport.java
@@ -3,7 +3,7 @@ package bendimitris;
 public interface DimitrisSupport {
 	
 	public void printBoard();
-	public void setUpBoard();
+	public void setUpBoard(int width, int hieght);
 	public void moveTeacher();
 	public void startGame();
 	public String[][] getBoard();
