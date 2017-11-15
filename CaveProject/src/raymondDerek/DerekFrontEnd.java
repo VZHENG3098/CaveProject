58edcaaee49ca12bb3f66e81e9e74b68813998c6
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-15 14:42:32 -0500

    started plots

--- CaveProject/src/raymondDerek/DerekFrontEnd.java
+++ CaveProject/src/raymondDerek/DerekFrontEnd.java
@@ -5,28 +5,27 @@ import caveExplorer.CaveExplorer;
 public class DerekFrontEnd implements RaymondSupporter {
 
 	private DerekSupporter backend;
-	
+
 	public static final void main(String[] args) {
 		DerekFrontEnd demo = new DerekFrontEnd();
 		demo.play();
-		
+
 	}
-	
+
 	public DerekFrontEnd() {
 		backend = new RaymondBackEnd(this);
-		
+
 	}
-	
-	public void play(){
-    while(backend.stillPlaying()){
-        displayBoard();
-    }
-}
+
+	public void play() {
+		while (backend.stillPlaying()) {
+			displayBoard();
+		}
+	}
 
 	public void displayBoard() {
+		RaymondDerekPlot[][] plots = backend.getPlots();
 		
 	}
-	
-	
+
 }
- 
--- CaveProject/src/raymondDerek/DerekSupporter.java
+++ CaveProject/src/raymondDerek/DerekSupporter.java
@@ -6,4 +6,6 @@ public interface DerekSupporter {
 
 	boolean stillPlaying();
 
+	RaymondDerekPlot[][] getPlots();
+
 }
