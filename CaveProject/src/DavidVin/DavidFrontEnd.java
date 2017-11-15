d62568bd0b63bcc326da39d2c7450b5c7e23509e
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-11-15 15:31:39 -0500

    update board and display and points

--- CaveProject/src/DavidVin/DavidFrontEnd.java
+++ CaveProject/src/DavidVin/DavidFrontEnd.java
@@ -1,10 +1,77 @@
 package DavidVin;
 
+import java.util.Scanner;
+
 public class DavidFrontEnd implements vincentSupport {
-	public static void play()
+	
+	private static Scanner inputSource = new Scanner(System.in);
+
+	public static boolean victory;
+	public static int row;
+	public static int col;
+	
+	
+	public DavidFrontEnd()
 	{
-			VincentBackEnd.startArray();
+		victory = false;
+		play();
+		
+		
+	}
+	public static String getInput(){
+		
+		return inputSource.nextLine();
 	}
 	
+	public static void play()
+	{
+		VincentBackEnd.startArray();
+		while(victory==false){
+			plot[][] plot = VincentBackEnd.getPlot();
+	    	System.out.println("Flip two cards to see if you have a match.");
+	    	displayBoard(plot);
+	    	
+	    	String firstCoordinate = getCords(plot); // format is 5,5 , is a string
+	    	String SecondCoordinate = getCords(plot);
+	    	
+	    	
+	    	VincentBackEnd.checkAnswer(firstCoordinate, SecondCoordinate);
+	    	
+	    	
+	    	
+	    	System.out.println(VincentBackEnd.getPoints());
+	    	int turns = VincentBackEnd.getTurns();
+	    	System.out.println(turns);
+	    	
+		}
+	}
+	
+	public static String getCords(plot[][] arr)
+	{
+		boolean finish = false;
+		String coordinate = getInput();
+		while(finish = false) {
+			if(VincentBackEnd.isCorrectFormat(coordinate, arr)) {
+				finish = true;
+				return coordinate;
+			}else {
+				System.out.println("Please type the coordinate like this example 5,5");
+				coordinate = getInput();
+			}
+		}
+		return coordinate;
+	}
+	
+	public static void displayBoard(plot[][] plot)
+	{
+		for(int row = 0; row < plot.length; row++){
+			for(int col = 0; col < plot[row].length; col++){
+				if(plot[row][col].isRevealed()){
 
+				}else{
+					System.out.print(".");
+				}
+			}
+		}
+	}
 }
