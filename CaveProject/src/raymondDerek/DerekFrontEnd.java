78b9633401b33a809382fa465008a3c0182b1205
dlius <dlius@192.168.0.15>
2017-11-16 00:05:08 -0500

    midnight update userinput

--- CaveProject/src/raymondDerek/DerekFrontEnd.java
+++ CaveProject/src/raymondDerek/DerekFrontEnd.java
@@ -1,11 +1,16 @@
 package raymondDerek;
 
+import java.util.Scanner;
+
 import caveExplorer.CaveExplorer;
 
 public class DerekFrontEnd implements RaymondSupporter {
 
 	private DerekSupporter backend;
-
+	private static Scanner inputSource = new Scanner(System.in);
+	private int playerPos;
+	private int a;
+	
 	public static final void main(String[] args) {
 		DerekFrontEnd demo = new DerekFrontEnd();
 		demo.play();
@@ -17,11 +22,24 @@ public class DerekFrontEnd implements RaymondSupporter {
 
 	public void play() {
 		while (backend.stillPlaying()) {
+			userInput();
 			displayBoard();
 			break;
-//			userInput();
+			
 //			updateMap();
 		}
+	}
+
+	private int userInput() {
+		CaveExplorer.print("Please type which box you would like to goto.");
+		String answer = getInput();
+		for(int i =0;i<answer.length();i++) {
+			if(isInteger(answer)) {
+				a = i;
+			}
+		}
+		playerPos = a;
+		return playerPos;
 	}
 
 	public void displayBoard() {
@@ -42,5 +60,21 @@ public class DerekFrontEnd implements RaymondSupporter {
 		}
 
 	}
+	
+	public static String getInput(){
+		return inputSource.nextLine();
+	}
 
+	public static boolean isInteger(String str) {
+	    try {
+	        Integer.parseInt(str);
+	        return true;
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
+	}
+	
+	public int getPlayerPos() {
+		return playerPos;
+	}
 }
