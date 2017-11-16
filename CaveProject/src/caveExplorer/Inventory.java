f8567b3950e3be1a30edfa5894ef8920fb08002e
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-11-16 15:32:28 -0500

    board

--- CaveProject/src/caveExplorer/Inventory.java
+++ CaveProject/src/caveExplorer/Inventory.java
@@ -1,61 +1,94 @@
-package caveExplorer;
+package DavidVin;
 
-public class Inventory {
+import java.util.Scanner;
 
-	private String map;
-	private int health;
-	private int gold;
-	public Inventory() {
-		health = 50;
-		gold = 0;
-		updateMap();
+public class DavidFrontEnd implements vincentSupport {
+	
+	private static Scanner inputSource = new Scanner(System.in);
 
+	public static boolean victory;
+	public static int row;
+	public static int col;
+	
+	
+	public DavidFrontEnd()
+	{
+		victory = false;
+		play();
+		
+		
+	}
+	public static String getInput(){
+		
+		return inputSource.nextLine();
 	}
 	
-	public void updateMap() {
-		map = " ";
-		for(int i = 0; i < CaveExplorer.caves[0].length - 1; i++) {
-			map += "____";
+	public static void play()
+	{
+		VincentBackEnd.startArray();
+		plot[][] plot = VincentBackEnd.getPlot();
+		displayBoard(plot);
+		
+		while(victory==false){
+			
+	    	System.out.println("\n"+"Flip two cards to see if you have a match.");
+	    	
+	    	
+	    	String firstCoordinate = getCords(plot); // format is 5,5 , is a string
+	    	displayBoard(plot); // displays letter in the plot
+	    	System.out.println("\nEnter your second coordinate.");                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+	    	String SecondCoordinate = getCords(plot);
+	    	displayBoard(plot);
+	    	
+	    	
+	    	VincentBackEnd.checkAnswer(firstCoordinate, SecondCoordinate);
+	    	
+	    	if(VincentBackEnd.checkAnswer(firstCoordinate, SecondCoordinate)==true)
+	    	{
+	    		System.out.println("You gain ten points");
+	    	}
+	    	
+	    	
+	    	System.out.println("\nYou currently have "+VincentBackEnd.getPoints()+" points");
+	    	int turns = VincentBackEnd.getTurns();
+	    	System.out.println("You have "+ turns + " flips left");
+	    	
+	    	
 		}
-		map += "___\n";
-		for(CaveRoom[] row: CaveExplorer.caves) {
-			for(int i = 0; i < 3; i++) {
-				String text = "";
-				for(CaveRoom cr: row) {
-					if(cr.getDoor(CaveRoom.WEST) != null && cr.getDoor(CaveRoom.WEST).isOpen()) {
-						text += " ";
-					} else {
-						text += "|";
-					}
-					if(i == 0) {
-						text += "   ";
-					} else if ( i == 1) {
-						text += " "+cr.getContents() + " ";
-					} else if( i == 2) {
-						if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen()) {
-							text += "   ";
-						} else {
-							text += "___"; //closed door or wall
-						}
-					}
-				}//last caveroom
-				text += "|";
-				map += text + "\n";
+	}
+	
+	public static String getCords(plot[][] arr)
+	{
+		boolean finish = false;
+		String coordinate = getInput();
+		while(finish == false) {
+			if(VincentBackEnd.isCorrectFormat(coordinate, arr)) {
+				finish = true;
+				return coordinate;
+			}else {
+				System.out.println("Please type the coordinate like this example 0,0 and each number is less than " + (arr.length-1));
+				coordinate = getInput();
+			}
+		}
+		return coordinate;
+	}
+	
+	public static void displayBoard(plot[][] arr)
+	{
+		for(int row = 0; row < arr.length; row++){
+			System.out.println("");
+			for(int col = 0; col < arr[row].length; col++)
+			{
+				if(arr[row][col].isRevealed()) {
+					System.out.print((arr[row][col].getLetter()));
+				}
+				else
+				{
+					System.out.print(".");
+				}
 			}
 		}
 	}
-	public void setHP() {
-		health = 100;
-	}
-	public String getDescription() {
-		return map ;
-		//return "h is nothing in your inventory.";
-	}
-	public String getHealth() {
-		return ""+health+"";
-	}
-	public void giveGold() {
-		gold += 100 ;
-	}
+	
+	
 }
-
