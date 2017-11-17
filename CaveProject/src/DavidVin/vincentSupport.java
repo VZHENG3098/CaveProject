63d2966c8a7ef2656233704ed3c93e1c38e09291
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-17 15:02:08 -0500

    Support

--- CaveProject/src/DavidVin/vincentSupport.java
+++ CaveProject/src/DavidVin/vincentSupport.java
@@ -1,5 +1,12 @@
 package DavidVin;
 
 public interface vincentSupport {
-
+	
+	public static String getCords(plot[][] arr) {
+		return null;
+	}
+	
+	public static void displayBoard(plot[][] arr) {
+	}
 }
+ 
