0509d666f18e7e24fb4571b5f928dd2a6b08690f
deltaalpha33 <dalexakos@gmail.com>
2017-11-20 20:00:54 -0500

    classwork

--- CaveProject/src/bendimitris/DimitrisBackend.java
+++ CaveProject/src/bendimitris/DimitrisBackend.java
@@ -1,6 +1,7 @@
 package bendimitris;
 
 import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
 
 public class DimitrisBackend implements BenSupport {
 	
@@ -9,6 +10,7 @@ public class DimitrisBackend implements BenSupport {
 	private int[] playerPosition;
 	
 	static String playerString = "X";
+	static String emptyString = " ";
 	
 	
 	
@@ -51,7 +53,11 @@ public class DimitrisBackend implements BenSupport {
 		// get user input/movement
 		
 		int direction = getInput();
+		boolean[] validMoves = calculateOpenSides(getPlayerPosition());
 		
+		if(validMoves[direction] == true) {
+			
+		}
 		
 	}
 	
@@ -68,10 +74,49 @@ public class DimitrisBackend implements BenSupport {
 		return error;
 	}
 	
-	public int[] calculateOpenSides(int[] pos) {
+	public boolean[] calculateOpenSides(int[] pos) {
+		//returns a bitmap of open sides
+		//the index of a direction is the numerical value of the direction in CaveRoom
+		boolean[] openSides = new boolean[4];
 		
-		int[] noSides = {-1,-1};
-		return noSides;
+		for(int i = 0; i < openSides.length; i++) {
+			openSides[i] = false;
+		}
+		
+		for(int row = pos[0]-1; row <= pos[0]+1; row++) {
+			for(int col = pos[1]-1; col <= pos[1]+1; col++) {
+				if(row >= 0 && row < board.length && col >= 0 && col < board[row].length) {
+					int rowOffset = row - pos[0];
+					int colOffset = col - pos[1];
+					//offsets cannot be equal, if they are then it is (0,0) or a diagonal, both are illegal moves
+					if(board[row][col] == DimitrisBackend.emptyString && rowOffset != colOffset){
+						if(row - pos[0] == -1) {
+							openSides[CaveRoom.NORTH] = true;
+						}
+						if(row - pos[0] == 1) {
+							openSides[CaveRoom.SOUTH] = true;
+						}
+						if(col - pos[1] == -1) {
+							openSides[CaveRoom.WEST] = true;
+						}
+						if(col - pos[1] == 1) {
+							openSides[CaveRoom.EAST] = true;
+						}
+					}
+				}
+			}
+			
+		}
+		
+		return openSides;
+	}
+	
+	public void moveEntity(String entityString, int[] pos, int Direction) {
+		//this function assumes that the bounds have been checked, the direction is valid, and the move is valid
+		//ie it will throw an out of bounds exception and will overwrite entities that are in the space being moved to
+		//the appropriate helper method( calcualteOpenSides does all of this checking and should be used)
+		
+		board[pos[0], pos[1]] = entityString;
 	}
 	
 	public int getInput() {
