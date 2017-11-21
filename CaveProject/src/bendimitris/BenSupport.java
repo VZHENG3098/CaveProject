2fa74408a7c716059b792404f2cfc65d9d2f484d
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-21 15:12:17 -0500

    changed interface for adding people

--- CaveProject/src/bendimitris/BenSupport.java
+++ CaveProject/src/bendimitris/BenSupport.java
@@ -3,4 +3,8 @@ package bendimitris;
 public interface BenSupport 
 {
 	public void runGame();
+	
+	public String[][] addLunchCounter(String[][] board);
+	public String[][] populatePeople(String[][] board, int sizeOfLine);
+	
 }
--- CaveProject/src/bendimitris/DimitrisBackend.java
+++ CaveProject/src/bendimitris/DimitrisBackend.java
@@ -7,8 +7,11 @@ public class DimitrisBackend implements BenSupport {
 	private int totalSec;
 	private String[][] board;
 	private int[] playerPosition;
+	private int[][] line;
 	
 	static String playerString = "X";
+	static String emptyString = "";
+	
 	
 	
 	
@@ -22,18 +25,13 @@ public class DimitrisBackend implements BenSupport {
 	public void runGame() {
 		//main loop
 		
-		int gameLen = 180; //Seconds the game will run (180s = 3min)
+		int maxTurns = 20;
+		int cnt = 0;
 		
-		//initialized timer
-		startTimer();
-		
-		
-		while(totalSec < gameLen) {//if player takes a long time to decide, then scanner will block using up precious time
+		while(cnt < maxTurns) {//if player takes a long time to decide, then scanner will block using up precious time
 			this.frontend.printBoard(); //print the board each turn
 			executeTurn(); //then update state
 		}
-		
-		//loop runs every x seconds until the time is larger than gameLen
 		
 	}
 	
@@ -41,6 +39,7 @@ public class DimitrisBackend implements BenSupport {
 		
 		frontend.moveTeacher(); //first ben moves teacher
 		board = frontend.getBoard(); //then I get the updated state
+		playerPosition = getPlayerPosition(); //get the player posistion for later stages
 		movePeople(); //using this updated state I move p
 		movePlayer();
 		
@@ -75,22 +74,53 @@ public class DimitrisBackend implements BenSupport {
 	}
 	
 	public int getInput() {
-		String input = caveExplorer.CaveExplorer.in.nextLine();
+		String input = CaveExplorer.in.nextLine();
 		if(input.length() == 1) {
 			return "wdsae".indexOf(input);
 		}
 		return -1;
 		
 	}
-
-	public void movePeople() {
-		// moves all of the people preserving the line
+	
+	public void moveEnitity(int[] pos, int direction) {
 		
 	}
 
-	public void startTimer() {
-		// starts timer
+	public void movePeople() {
+		//people move in a line, occasionally people will leave a hole in the line (ie the person will not move)
+		//front of the line is closest to the lunch counter
 		
+
+		
+	}
+
+	@Override
+	public String[][] addLunchCounter(String[][] board) {
+		if(board.length != 0 && board[0].length != 0) {
+			board[0][board[0].length-1] = ")"; //the bottom left corner is the lunch counter
+		}
+		return board;
+	}
+
+	@Override
+	public String[][] populatePeople(String[][] board, int sizeOfLine) {
+		line = new int[sizeOfLine][2]; //each person has a coordinate
+		int cnt = 0;
+		while(hasFreeSpace(board) && sizeOfLine < cnt) {
+			cnt++;
+		}
+		return board;
+	}
+	
+	public boolean hasFreeSpace(String[][] board) {
+		for(int row = 0; row < board.length; row++) {
+			for(int col = 0; col < board[row].length; col++) {
+				if(board[row][col] == DimitrisBackend.emptyString) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 }
