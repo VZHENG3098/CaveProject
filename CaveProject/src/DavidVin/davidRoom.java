068d0feb15da93081c4d6bc66f7638f171d7ec85
letgo09 <letgo09@VincentZheng>
2017-11-26 17:47:48 -0500

    English Room Update

--- CaveProject/src/DavidVin/davidRoom.java
+++ CaveProject/src/DavidVin/davidRoom.java
@@ -26,9 +26,11 @@ public class davidRoom extends CaveRoom {
 
 
 		if(direction == 4) {
-			CaveExplorer.print("Press e to start");
+			if(CaveExplorer.inventory.getEssay() == true) {
 				
-			startGame();
+			}else {
+				DavidFrontEnd.play();
+			}
 		}
 		else 
 		{
@@ -47,13 +49,21 @@ public class davidRoom extends CaveRoom {
 	
 	public String getDescription()
 	{
+		if(CaveExplorer.inventory.getEssay() == true) {
+			return "\n You came prepare to english class today. So you can recover your stamina by sleeping";
+		}else
+			
 		return"\nWelcome to English Class."+
 		"\nYou forgot to do you summer reading assignment."+
 		"\nYou ask your neighbor for the homework so you can copy it."+
 		"\nHe said he will only let you copy off him, if you are able to earn 60 points in a matching game."+
 		"\n"+"The rules are simple flip two cards to see if you have a match."+
 		"\n"+"Each match is worth 10 points."+
-		"\n"+"You have 25 turns to find the matching pairs, or else you lose.";
+		"\n"+"You have 25 turns to find the matching pairs, or else you lose."+
+		"\n"+"Press E to start!";
 	}
-
+	public String getContents() {
+		return "ENG";
+		
+	}
 }
